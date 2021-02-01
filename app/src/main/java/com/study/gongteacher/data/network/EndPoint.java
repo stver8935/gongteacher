bb3ea1fe64a3ab957db001d0640c58427aa19a8c
Administrator <stver8935>
2021-02-01 16:18:06 +0900

    21-02-01

--- app/src/main/java/com/study/gongteacher/data/network/EndPoint.java
+++ app/src/main/java/com/study/gongteacher/data/network/EndPoint.java
@@ -8,7 +8,7 @@ public class EndPoint {
 
     public static final String SERVER_BASE_URL =  "http://13.125.130.142/GONG_TEACHER/";
 
-    public static final String FACEBOOK_LOGIN_URL = "쌸라쌸라";
+    public static final String FACEBOOK_LOGIN_URL = "";
     public static final String KAKAO_LOGIN_URL = "";
     public static final String NAVER_LOGIN_URL = "";
     public static final String GOOGLE_LOGIN_URL = "";
